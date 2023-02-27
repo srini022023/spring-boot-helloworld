73252de9f17af24ef0517798cd7c5f6bfc0938af
Srinivasarao Polagani <srinipolagani2@gmail.com>
2023-02-26 21:02:32 -0500

    Second Commit

---
+++ src/test/java/com/sample/example/springboothelloworld/SpringBootHelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sample.example.springboothelloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootHelloworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

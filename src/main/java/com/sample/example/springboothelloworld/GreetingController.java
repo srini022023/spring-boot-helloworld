c11e1ad55a5de03946475294c452ddd3eccbb6e3
srini022023 <126432198+srini022023@users.noreply.github.com>
2023-02-26 22:25:23 -0500

    Update GreetingController.java

--- src/main/java/com/sample/example/springboothelloworld/GreetingController.java
+++ src/main/java/com/sample/example/springboothelloworld/GreetingController.java
@@ -12,6 +12,6 @@ public class GreetingController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Hello World! Your application is running";
+        return "Hey Greetings API, you are too good";
     }
 }
